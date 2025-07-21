86bc839667312f35e4a264da063bbedd1d10ac46
ynsatay <ynsatay3734@gmail.com>
2025-07-21 18:43:42 +0300

    .

--- index.js
+++ index.js
@@ -20,8 +20,15 @@ dotenv.config({ path: path.resolve('../.env') }); // gerekirse './.env' de olabi
 const app = express();
 const port = process.env.PORT;
 
-app.use(cors()); // CORS middleware'ini kullanın
-app.use(express.json()); // JSON parselleme
+app.use(cors({ origin: "*"}));
+
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
+// JSON ve URL-encoded veri ayrıştırma middleware'leri
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 methods(app); // API metotlarını ekleyin
 methodsanimals(app);
