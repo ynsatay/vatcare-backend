976926fd1eba39876752a4bf325c07906227150d
ynsatay <ynsatay3734@gmail.com>
2025-07-25 17:04:52 +0300

    api düzenleme

--- methods/vaccine.js
+++ methods/vaccine.js
@@ -218,7 +218,7 @@ function methodVaccine(app) {
                     'vp.notes',
                     'vp.is_applied',
                     'vp.animal_id',
-                    'm.name as vaccine_name',
+                    'm.animalname as vaccine_name',
                     'ua.name as animal_name',
                     connection.raw("CONCAT(u.name, ' ', u.surname) as owner_name")
                 )
