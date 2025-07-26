dc8563b8e55127623b8da8b1ad8026d807699fb6
ynsatay <ynsatay3734@gmail.com>
2025-07-26 21:30:52 +0300

    düzenleme

--- methods/index.js
+++ methods/index.js
@@ -14,7 +14,7 @@ const upload = multer({ storage: storage });
 function methods(app) {
     //Giriş İşlemleri
     app.post('/api/login', (req, res) => {
-        const { username, password } = req.body;
+        const { username, password, office_id  } = req.body;
 
         if (!username || !password) {
             return res.status(400).json({ error: 'Kullanıcı adı ve şifre gereklidir', status: 'error' });
@@ -24,7 +24,7 @@ function methods(app) {
             .where(function () {
                 this.where('uname', username).orWhere('email', username);
             })
-            .andWhere('off_id', off_id)
+            .andWhere('off_id', office_id )
             .first()
             .then((user) => {
                 if (!user) {
